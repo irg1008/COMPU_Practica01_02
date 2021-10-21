2cdfd797ad99a4badc12028975474a58dfb0d5a3
irg1008 <ivansudevlop@gmail.com>
2021-10-21 17:38:02 +0200

    Fitness changed

--- src/sol/EvolCycle.py
+++ src/sol/EvolCycle.py
@@ -2,17 +2,20 @@ from deap import tools
 from deap import algorithms
 from EvalSol import eval_ind
 
+
 def config_alg(toolbox, config, rides, TOURNSIZE, INDPB):
     def eval(ind): return eval_ind(ind, config, rides)
 
+    F = config[0]
+
     toolbox.register("select", tools.selTournament, tournsize=int(TOURNSIZE))
     toolbox.register("mate", tools.cxOnePoint)
-    toolbox.register("mutate", tools.mutShuffleIndexes, indpb=INDPB)
+    toolbox.register("mutate", tools.mutUniformInt, indpb=INDPB, low=0, up=F-1)
     toolbox.register("evaluate", eval)
 
 
 def evolve(toolbox, stats, config, rides, CXPB=0.95, MUTPB=0.075, NGEN=15, NIND=50, TOURNSIZE=3, INDPB=0.05):
-    
+
     config_alg(toolbox, config, rides, TOURNSIZE, INDPB)
 
     pop = toolbox.population(n=int(NIND))
